46301b5be8e123eadbfacaa83fe3e437c65b2990
Eduardo Pereira <eduardo.soares@pagar.me>
2020-05-02 22:52:12 -0300

    Update README

--- pypika_gis/spatialtypes.py
+++ pypika_gis/spatialtypes.py
@@ -21,12 +21,12 @@ def AsText(term, *args):
     return Function("ST_AsText", term, *args)
 
 
-def Buffer(term, length, *args):
-    return Function("ST_Buffer", term, length, *args)
-
-
 def Boundary(geom, *args):
     return Function("ST_Boundary", geom, *args)
+
+
+def Buffer(term, length, *args):
+    return Function("ST_Buffer", term, length, *args)
 
 
 def Centroid(term, *args):
@@ -41,8 +41,20 @@ def ClosestPoint(geomA, geomB, *args):
     return Function("ST_ClosestPoint", geomA, geomB, *args)
 
 
+def CoveredBy(geomA, geomB, *args):
+    return Function("ST_CoveredBy", geomA, geomB, *args)
+
+
+def Covers(geomA, geomB, *args):
+    return Function("ST_Covers", geomA, geomB, *args)
+
+
 def Difference(geomA, geomB, *args):
     return Function("ST_Difference", geomA, geomB, *args)
+
+
+def Dimension(geom, *args):
+    return Function("ST_Dimension", geom, *args)
 
 
 def Disjoint(geomA, geomB, *args):
@@ -53,6 +65,14 @@ def Distance(geomA, geomB, *args):
     return Function("ST_Distance", geomA, geomB, *args)
 
 
+def DWithin(geomA, geomB, distance, use_spheroid=False):
+    return Function("ST_DWithin", geomA, geomB, distance, use_spheroid)
+
+
+def Equals(geomA, geomB, *args):
+    return Function("ST_Equals", geomA, geomB, *args)
+
+
 def Envelope(term, *args):
     return Function("ST_Envelope", term, *args)
 
@@ -61,8 +81,8 @@ def Extent(term, *args):
     return Function("ST_Extent", term, *args)
 
 
-def GeomFromGeoJSON(term, *args):
-    return Function("ST_GeomFromGeoJSON", term, *args)
+def GeoHash(term, *args):
+    return Function("ST_GeoHash", term, *args)
 
 
 def GeogFromGeoJSON(term, *args):
@@ -77,16 +97,20 @@ def GeogFromWKB(wkb, *args):
     return Function("ST_GeogFromWKB", wkb, *args)
 
 
-def GeogPointFromGeoHash(geohash, *args):
-    return Function("ST_GeogPointFromGeoHash", geohash, *args)
-
-
 def GeogPoint(long, lat, *args):
     return Function("ST_GeogPoint", long, lat, *args)
 
 
-def GeoHash(term, *args):
-    return Function("ST_GeoHash", term, *args)
+def GeogPointFromGeoHash(geohash, *args):
+    return Function("ST_GeogPointFromGeoHash", geohash, *args)
+
+
+def GeomFromGeoJSON(term, *args):
+    return Function("ST_GeomFromGeoJSON", term, *args)
+
+
+def Length(geom, *args):
+    return Function("ST_Length", geom, *args)
 
 
 def Intersection(geomA, geomB, *args):
@@ -97,28 +121,32 @@ def Intersects(geomA, geomB, *args):
     return Function("ST_Intersects", geomA, geomB, *args)
 
 
-def IsEmpty(term, *args):
-    return Function("ST_IsEmpty", term, *args)
-
-
 def IsCollection(term, *args):
     return Function("ST_IsCollection", term, *args)
+
+
+def IsEmpty(term, *args):
+    return Function("ST_IsEmpty", term, *args)
 
 
 def IsValid(term, *args):
     return Function("ST_IsValid", term, *args)
 
 
-def MakePoint(long, lat, *args):
-    return Function("ST_MakePoint", long, lat, *args)
-
-
 def MakeLine(*args):
     return Function("ST_MakeLine", *args)
 
 
+def MakePoint(long, lat, *args):
+    return Function("ST_MakePoint", long, lat, *args)
+
+
 def MakePolygon(*args):
     return Function("ST_MakePolygon", *args)
+
+
+def NumPoints(geom, *args):
+    return Function("ST_NumPoints", geom, *args)
 
 
 def Perimeter(geom, *args):
@@ -133,28 +161,12 @@ def SetSRID(geom, epsg, *args):
     return Function("ST_SetSRID", geom, epsg, *args)
 
 
-def Union(geomA, geomB, *args):
-    return Function("ST_Union", geomA, geomB, *args)
-
-
-def CoveredBy(geomA, geomB, *args):
-    return Function("ST_CoveredBy", geomA, geomB, *args)
-
-
-def Covers(geomA, geomB, *args):
-    return Function("ST_Covers", geomA, geomB, *args)
-
-
-def Equals(geomA, geomB, *args):
-    return Function("ST_Equals", geomA, geomB, *args)
-
-
 def Touches(geomA, geomB, *args):
     return Function("ST_Touches", geomA, geomB, *args)
 
 
-def DWithin(geomA, geomB, distance, use_spheroid=False):
-    return Function("ST_DWithin", geomA, geomB, distance, use_spheroid)
+def Union(geomA, geomB, *args):
+    return Function("ST_Union", geomA, geomB, *args)
 
 
 def Within(geomA, geomB, *args):
@@ -163,18 +175,6 @@ def Within(geomA, geomB, *args):
 
 def X(term, *args):
     return Function("ST_X", term, *args)
-
-
-def Dimension(geom, *args):
-    return Function("ST_Dimension", geom, *args)
-
-
-def NumPoints(geom, *args):
-    return Function("ST_NumPoints", geom, *args)
-
-
-def Length(geom, *args):
-    return Function("ST_Length", geom, *args)
 
 
 def Y(term, *args):
