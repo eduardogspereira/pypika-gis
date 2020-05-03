61f30f242c78d74925791d28c49f26a374c3fb80
Eduardo Pereira <eduardo.soares@pagar.me>
2020-05-02 22:05:51 -0300

    Add *args on all functions to allow extra arguments

--- pypika_gis/spatialtypes.py
+++ pypika_gis/spatialtypes.py
@@ -1,109 +1,109 @@
 from pypika.terms import Function
 
 
-def Area(term):
-    return Function("ST_Area", term)
+def Area(term, *args):
+    return Function("ST_Area", term, *args)
 
 
-def AsBinary(term):
-    return Function("ST_AsBinary", term)
+def AsBinary(term, *args):
+    return Function("ST_AsBinary", term, *args)
 
 
-def AsGeoJSON(term):
-    return Function("ST_AsGeoJSON", term)
+def AsGeoJSON(term, *args):
+    return Function("ST_AsGeoJSON", term, *args)
 
 
-def AsMVT(term):
-    return Function("ST_AsMVT", term)
+def AsMVT(term, *args):
+    return Function("ST_AsMVT", term, *args)
 
 
-def AsText(term):
-    return Function("ST_AsText", term)
+def AsText(term, *args):
+    return Function("ST_AsText", term, *args)
 
 
-def Buffer(term, length):
-    return Function("ST_Buffer", term, length)
+def Buffer(term, length, *args):
+    return Function("ST_Buffer", term, length, *args)
 
 
-def Centroid(term):
-    return Function("ST_Centroid", term)
+def Centroid(term, *args):
+    return Function("ST_Centroid", term, *args)
 
 
-def Contains(geomA, geomB):
-    return Function("ST_Contains", geomA, geomB)
+def Contains(geomA, geomB, *args):
+    return Function("ST_Contains", geomA, geomB, *args)
 
 
-def Difference(geomA, geomB):
-    return Function("ST_Difference", geomA, geomB)
+def Difference(geomA, geomB, *args):
+    return Function("ST_Difference", geomA, geomB, *args)
 
 
-def Disjoint(geomA, geomB):
-    return Function("ST_Disjoint", geomA, geomB)
+def Disjoint(geomA, geomB, *args):
+    return Function("ST_Disjoint", geomA, geomB, *args)
 
 
-def Distance(geomA, geomB):
-    return Function("ST_Distance", geomA, geomB)
+def Distance(geomA, geomB, *args):
+    return Function("ST_Distance", geomA, geomB, *args)
 
 
-def Envelope(term):
-    return Function("ST_Envelope", term)
+def Envelope(term, *args):
+    return Function("ST_Envelope", term, *args)
 
 
-def Extent(term):
-    return Function("ST_Extent", term)
+def Extent(term, *args):
+    return Function("ST_Extent", term, *args)
 
 
-def GeomFromGeoJSON(term):
-    return Function("ST_GeomFromGeoJSON", term)
+def GeomFromGeoJSON(term, *args):
+    return Function("ST_GeomFromGeoJSON", term, *args)
 
 
-def GeogPoint(long, lat):
-    return Function("ST_GeogPoint", long, lat)
+def GeogPoint(long, lat, *args):
+    return Function("ST_GeogPoint", long, lat, *args)
 
 
-def GeoHash(term):
-    return Function("ST_GeoHash", term)
+def GeoHash(term, *args):
+    return Function("ST_GeoHash", term, *args)
 
 
-def Intersection(geomA, geomB):
-    return Function("ST_Intersection", geomA, geomB)
+def Intersection(geomA, geomB, *args):
+    return Function("ST_Intersection", geomA, geomB, *args)
 
 
-def Intersects(geomA, geomB):
-    return Function("ST_Intersects", geomA, geomB)
+def Intersects(geomA, geomB, *args):
+    return Function("ST_Intersects", geomA, geomB, *args)
 
 
-def IsEmpty(term):
-    return Function("ST_IsEmpty", term)
+def IsEmpty(term, *args):
+    return Function("ST_IsEmpty", term, *args)
 
 
-def IsValid(term):
-    return Function("ST_IsValid", term)
+def IsValid(term, *args):
+    return Function("ST_IsValid", term, *args)
 
 
-def MakePoint(long, lat):
-    return Function("ST_MakePoint", long, lat)
+def MakePoint(long, lat, *args):
+    return Function("ST_MakePoint", long, lat, *args)
 
 
-def Point(long, lat):
-    return Function("ST_Point", long, lat)
+def Point(long, lat, *args):
+    return Function("ST_Point", long, lat, *args)
 
 
-def SetSRID(geom, epsg):
-    return Function("ST_SetSRID", geom, epsg)
+def SetSRID(geom, epsg, *args):
+    return Function("ST_SetSRID", geom, epsg, *args)
 
 
-def Within(geomA, geomB):
-    return Function("ST_Within", geomA, geomB)
+def Within(geomA, geomB, *args):
+    return Function("ST_Within", geomA, geomB, *args)
 
 
-def X(term):
-    return Function("ST_X", term)
+def X(term, *args):
+    return Function("ST_X", term, *args)
 
 
-def Y(term):
-    return Function("ST_Y", term)
+def Y(term, *args):
+    return Function("ST_Y", term, *args)
 
 
-def Z(term):
-    return Function("ST_Z", term)
+def Z(term, *args):
+    return Function("ST_Z", term, *args)
